2ad66e784fc622650b7f9d46cd07fb7c8a92ade8
jordangunther <jordankgunther@gmail.com>
2015-10-08 21:07:19 -0600

    got tweet to display

--- js/animations.js
+++ js/animations.js
@@ -25,7 +25,47 @@ $(document).ready(function(){
 		}
 	});
 	$('#tweet-submit').on('click', function (){
-		alert('you clicked tweet!')
-		$('.tweet').prepend($('div.stream div:first'));
+		$('.tweet').prepend(
+						+'<div class="content">'
+							+'<img class="avatar" src=file:///Users/jordangunther/Desktop/DevMountain/week4/twitter-clone/img/alagoon.jpg />'
+							+'<strong class="fullname">Jordan Kurt Gunther </strong>'
+							+'<span class="username">@jogunthe</span>'
+							+'<p class="tweet-text">'+ tweetTxtBox.val() +'</p>'
+							+'<div class="tweet-actions">'
+								+'<ul>'
+									+'<li><span class="icon action-reply"></span> Reply</li>'
+									+'<li><span class="icon action-retweet"></span> Retweet</li>'
+									+'<li><span class="icon action-favorite"></span> Favorite</li>'
+									+'<li><span class="icon action-more"></span> More</li>'
+								+'</ul>'
+							+'</div>'
+							+'<div class="stats">'
+								+'<div class="retweets">'
+									+'<p class="num-retweets">30</p>'
+									+'<p>RETWEETS</p>'
+								+'</div>'
+								+'<div class="favorites">'
+									+'<p class="num-favorites">6</p>'
+									+'<p>FAVORITES</p>'
+								+'</div>'
+								+'<div class="users-interact">'
+									+'<div>'
+										+'<img src="img/alagoon.jpg" />'
+										+'<img src="img/vklimenko.jpg" />'
+									+'</div>'
+								+'</div>'
+								+'<div class="time">'
+									+'1:04 PM - 19 Sep 13'
+								+'</div>'
+							+'</div>'
+							+'<div class="reply">'
+								+'<img class="avatar" src="img/alagoon.jpg" />'
+								+'<textarea class="tweet-compose" placeholder="Reply to @jogunthe"/></textarea>'
+							+'</div>'
+						+'</div>'
+					+'</div>'
+		)					
+	});
+	$('.tweet-actions').hide();
 	});
 });
